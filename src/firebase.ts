42e72a1db7529b52ca472384e5748a55b881ee1c
kyagitk-dot <k.yagi.tk@gmail.com>
2026-06-24 15:37:13 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDDg_RiW9TDszcKrAlp18jbJNUpgybfXxM",
+  authDomain: "hr-app-c0a12.firebaseapp.com",
+  projectId: "hr-app-c0a12",
+  storageBucket: "hr-app-c0a12.firebasestorage.app",
+  messagingSenderId: "230219971223",
+  appId: "1:230219971223:web:84ca5d399e7a687e3947d2"
 };
 
 const app = initializeApp(firebaseConfig);
